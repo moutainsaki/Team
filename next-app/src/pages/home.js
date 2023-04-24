a279ded768005fa182a3454b1875356345914a8a
Cyrus Man <114724876+cyrusmanosa@users.noreply.github.com>
2023-04-24 12:21:15 +0900

    Update - next

---
+++ next-app/src/pages/home.js
@@ -0,0 +1,18 @@
+import styles from "../styles/page.module.css"
+
+export default function home() {
+  return (
+    <div className={styles.container}>
+    <h1>（会社の名前</h1>
+    <h1>災害時連絡掲示板</h1>
+        <h1 className={styles.title}>登録完了</h1>
+
+        <main>
+          <p className={styles.question}>安全な場所に避難して周囲の安全を確保してください。</p>
+          <a href="example.js" className={styles.companybtn}>社員の状況一覧</a><br></br>
+          <a href="example.js" className={styles.managementbtn}>管理</a><br></br>
+          <a href="example.js" className={styles.logoutbtn}>ログアウト</a><br></br>
+        </main>
+    </div>
+  );
+}
