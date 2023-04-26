beb07348b0500e27437d01fc69bc1576c2330428
Cyrus Man <114724876+cyrusmanosa@users.noreply.github.com>
2023-04-26 16:43:11 +0900

    Update HTML と CSS の内容は完成しました

--- 災害時連絡掲示板Web/DB.php
+++
@@ -1,9 +0,0 @@
-<?php
-$ERROR="";
-$dbConnection = mysqli_connect("localhost","dbuser","ecc","test");
-
-if( mysqli_connect_errno () ){
-    print "Failed to connect to database" . mysqli_connect_error();
-    exit();
-}
-?>
